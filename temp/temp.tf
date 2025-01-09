65cf62bb0acbfeb53a60df7834d80227aa726649
Regina117 <r.kunafina@gmail.com>
2025-01-09 21:01:01 +0300

    vpc

--- temp/temp.tf
+++ temp/temp.tf
@@ -47,9 +47,9 @@ resource "yandex_vpc_network" "network_terraform" {
   name = "net_terraform"
 }
  
-resource "yandex_vpc_subnet" "subnet_terraform" {
+resource "yandex_vpc_subnet" "default" {
   name           = "sub_terraform"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.network_terraform.id
-  v4_cidr_blocks = ["192.168.15.0/24"]
+  zone           = "ru-central1-d"
+  network_id     = yandex_vpc_network.default.id
+  v4_cidr_blocks = ["10.0.0.0/24"]
 }
