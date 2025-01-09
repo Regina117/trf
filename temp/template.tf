3a7ddbd1c43e2419d7f23d9d1602f1fa9aec6bd6
Regina117 <r.kunafina@gmail.com>
2025-01-09 20:48:05 +0300

    add

--- temp/template.tf
+++ temp/template.tf
@@ -8,10 +8,10 @@ terraform {
 }
 
 provider "yandex" {
-  token     = var.yandex_token
-  cloud_id  = var.yandex_cloud_id
-  folder_id = var.yandex_folder_id
-  zone      = var.yandex_zone
+  token     = "y0__wgBENPV_IUCGMHdEyCis4b-EZr_VyNwznnybCcPlXNywg3E7sZf"
+  cloud_id  = "b1gmclt461srvopvr7i7"
+  folder_id = "b1g877q94b2773okudu0"
+  zone      = "ru-central1-d"
 }
 
 resource "yandex_compute_instance" "vm" {
