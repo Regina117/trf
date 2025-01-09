a580f9dbd5bda35a9b100da9143f8d5260d3fc3c
Regina117 <r.kunafina@gmail.com>
2025-01-09 21:17:00 +0300

    update

--- temp/temp.tf
+++ temp/temp.tf
@@ -11,40 +11,40 @@ provider "yandex" {
   token     = "y0__wgBENPV_IUCGMHdEyCis4b-EZr_VyNwznnybCcPlXNywg3E7sZf"
   cloud_id  = "b1gmclt461srvopvr7i7"
   folder_id = "b1g877q94b2773okudu0"
-  zone      = "ru-central1-d"
 }
 
-data "yandex_compute_image" "ubuntu_image" {
-  family = "ubuntu-2004-lts"
+resource "yandex_compute_disk" "boot-disk" {
+  name = "boot-disk"
+  type = "network-hdd"
+  zone = "ru-central1-a"
+  size = "20"
+  image_id = "fd895e9j3al6len7lg24" 
 }
- 
-resource "yandex_compute_instance" "vm-test1" {
-  name = "test1"
- 
-  resources {
-    cores  = 2
-    memory = 2
-  }
- 
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu_image.id
-    }
-  }
- 
-  network_interface {
-    subnet_id = "yandex_vpc_network.network_terraform.id"
-    nat       = true
-  }
- 
-  metadata = {
-    user-data = "regina:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIBb3GHLLen70A0nwelg/oTdfw0P2bRGVAtMEwbhOBwQ"
-  }
- 
+resource "yandex_compute_instance" "linux-vm" {
+  name = "linux-vm"
+  platform_id = "standard-v3"
+  zone = "ru-central1-a"
+resources {
+  cores = 2
+  memory = 4
 }
-resource "yandex_vpc_subnet" "network_terraform" {
-  name           = "sub_terraform"
-  zone           = "ru-central1-d"
-  network_id     = "yandex_vpc_network.network_terraform.id"
-  v4_cidr_blocks = ["10.0.0.0/24"]
+boot_disk {
+  disk_id = yandex_compute_disk.boot-disk.id
+}
+network_interface {
+  subnet_id = yandex_vpc_subnet.subnet-1.id
+  nat = true
+}
+metadata = {
+  user-data = "#cloud-config\nusers:\n - name: devops\n groups: sudo\n shell: /bin/bash\n sudo: 'ALL=(ALL) NOPASSWD:ALL'\n ssh-authorized-keys:\n - ${file("~/.ssh/id_rsa.pub")}"
+}
+}
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+resource "yandex_vpc_subnet" "subnet-1" {
+  name = "subnet1"
+  zone = "ru-central1-a"
+  v4_cidr_blocks = ["192.168.10.0/24"]
+  network_id = yandex_vpc_network.network-1.id
 }
