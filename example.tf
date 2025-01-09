dbf5a0e926995e94a61ad0a098d3b31d839f3fed
Regina117 <r.kunafina@gmail.com>
2025-01-09 13:45:53 +0300

    update name

--- example.tf
+++ example.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "yandex" {
   token     = "y0__wgBENPV_IUCGMHdEyCis4b-EZr_VyNwznnybCcPlXNywg3E7sZf"
-  cloud-id  = "b1gmclt461srvopvr7i7"
-  folder-id = "b1g877q94b2773okudu0"
+  cloud_id  = "b1gmclt461srvopvr7i7"
+  folder_id = "b1g877q94b2773okudu0"
   zone      = "ru-central1-c"
 }
 
